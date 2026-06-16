1abd1fa00e946f56d075225fb3f886d4c274619e
Chris <paciottichris80@gmail.com>
2026-06-16 12:01:35 -0400

    yes

--- src/routes/keychain.js
+++ src/routes/keychain.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+const path = require('path');
 
 router.get('/fortnite/api/keychain/*', (req, res) => {
-    res.json({});
+    const keychainPath = path.join(__dirname, '../../responses/keychain.json');
+    if (fs.existsSync(keychainPath)) {
+        const keychain = JSON.parse(fs.readFileSync(keychainPath, 'utf8'));
+        res.json(keychain);
+    } else {
+        res.json([]);
+    }
 });
 
 router.post('/fortnite/api/keychain/*', (req, res) => {
-    res.json({});
+    const keychainPath = path.join(__dirname, '../../responses/keychain.json');
+    if (fs.existsSync(keychainPath)) {
+        const keychain = JSON.parse(fs.readFileSync(keychainPath, 'utf8'));
+        res.json(keychain);
+    } else {
+        res.json([]);
+    }
 });
 
 module.exports = router;
